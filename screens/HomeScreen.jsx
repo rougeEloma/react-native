91c138fb8c2219f5e057e90d55c812581a80e78c
ALAIN ELOMA <alainelomapro@gmail.com>
2024-09-07 14:19:13 +0100

    homescreen - beginning - pfp issue

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,56 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import {
+  AdjustmentsHorizontalIcon,
+  MagnifyingGlassIcon,
+  TrashIcon,
+} from "react-native-heroicons/solid";
+
+export default function HomeScreen() {
+
+  //  categories APIs
+
+  //   featured APIs
+
+  //   console.log("Featured Items", featured);
+
+  return (
+    <>
+      <SafeAreaView className=" bg-white">
+        <View className="bg-white">
+          <View className=" mt-2 mb-3 mx-4 flex-row items-center justify-between">
+              <TouchableOpacity>
+                <Image
+                  source={require("../assets/images/IMG_0616.jpg")}
+                  className=" w-14 h-14 object-cover rounded-full"
+                />
+              </TouchableOpacity>
+            <View className="flex-1">
+              <Text className=" font-bold text-base text-gray-800">
+                Salut Alino!
+              </Text>
+              <Text className="font-light text-xs text-gray-600">
+                Bonne apetit !
+              </Text>
+            </View>
+            <TouchableOpacity
+              className=" p-3 flex items-center justify-center rounded-full bg-gray-300"
+            >
+              <Text className=" absolute top-0 left-3 font-bold text-red-500">
+                0
+              </Text>
+              <TrashIcon size={22} color="black" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
